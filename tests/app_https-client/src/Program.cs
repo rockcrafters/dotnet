7744a94a8e315646f7eaaa494f8b56fd5f592866
Cristovao Cordeiro <cristovao.cordeiro@canonical.com>
2023-02-07 15:01:31 +0100

    feat: add .NET7 builds for Lunar and Kinetic

---
+++ tests/app_https-client/src/Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+using System.Net.Http;
+
+HttpClient hc = new HttpClient(new HttpClientHandler { ClientCertificateOptions = ClientCertificateOption.Automatic });
+Console.WriteLine(hc.GetStringAsync("https://httpbin.org/ip").GetAwaiter().GetResult());
